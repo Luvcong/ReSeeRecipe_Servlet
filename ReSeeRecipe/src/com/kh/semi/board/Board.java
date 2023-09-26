8254e7e08e60d4828e5bab8fda9503daefe4c01d
eclipsecandy <142475226+eclipsecandy@users.noreply.github.com>
2023-09-26 14:41:22 +0900

    Update Board.java
    커밋

--- ReSeeRecipe/src/com/kh/semi/board/Board.java
+++ ReSeeRecipe/src/com/kh/semi/board/Board.java
@@ -5,5 +5,13 @@ public class Board {
 	public void hi() {
 		String str = "안녕";
 	}
+	
+	public void rang() {
+		System.out.println("내가 먼저 작업할 거지롱");
+	}
+	
+	public void secondtest() {
+		System.out.println("여러개를 해볼거야");
+	}
 
 }
