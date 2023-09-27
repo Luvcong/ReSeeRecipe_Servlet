4738c3545d17f060768858e47b0cb1753bde31d8
JYChloeMok <137197469+JYChloeMok@users.noreply.github.com>
2023-09-27 17:03:55 +0900

    Create fields - MJY
    5:03p

---
+++ ReSeeRecipe/src/com/kh/semi/board/unsaved_recipe/model/vo/UnCookSteps.java
@@ -0,0 +1,20 @@
+package com.kh.semi.board.unsaved_recipe.model.vo;
+
+public class UnCookSteps {
+	
+	/*
+	UN_COOK_STEPS_NO	NUMBER
+	UN_COOK_STEPS_TITLE	VARCHAR2(100 BYTE)
+	UN_COOK_STEPS_CONTENT	VARCHAR2(1500 BYTE)
+	UN_COOK_STEPS_LEV	NUMBER
+	UN_RECIPE_NO	NUMBER
+	*/
+	
+	private int unCookStepsNo;
+	private String unCookStepsTitle;
+	private String unCookStepesContent;
+	private int unCookStepsLev;
+	
+	
+	
+}
