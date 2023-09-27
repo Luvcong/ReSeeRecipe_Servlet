a4dc4112532614126d39b03b0a29bdbe1e09b6a1
JYChloeMok <137197469+JYChloeMok@users.noreply.github.com>
2023-09-27 15:41:04 +0900

    Add VO fileds - MJY
    3:40p

--- ReSeeRecipe/src/com/kh/semi/board/recipe/model/vo/RecipeCategory.java
+++ ReSeeRecipe/src/com/kh/semi/board/recipe/model/vo/RecipeCategory.java
@@ -1,5 +1,12 @@
-package com.kh.semi.recipes.recipe.model.vo;
+package com.kh.semi.board.recipe.model.vo;
 
 public class RecipeCategory {
-
+	/*
+	RECIPE_CATEGORY_NO	NUMBER
+	RECIPE_CATEGORY_NAME	VARCHAR2(20 BYTE)
+	*/
+	
+	private int recipeCategoryNo;
+	private String recipeCategoryName;
+	
 }
