66995dfdf050f9f13f20e59f5a1760942e105190
JYChloeMok <137197469+JYChloeMok@users.noreply.github.com>
2023-09-27 16:20:37 +0900

    Create VO fields - MJY
    4:20p
    The fields for recipe, unsaved recipe and etc

--- ReSeeRecipe/src/com/kh/semi/board/recipe/model/vo/RecipeCategory.java
+++ ReSeeRecipe/src/com/kh/semi/board/recipe/model/vo/RecipeCategory.java
@@ -8,5 +8,35 @@ public class RecipeCategory {
 	
 	private int recipeCategoryNo;
 	private String recipeCategoryName;
+	public RecipeCategory() {
+		super();
+	}
+	public RecipeCategory(int recipeCategoryNo, String recipeCategoryName) {
+		super();
+		this.recipeCategoryNo = recipeCategoryNo;
+		this.recipeCategoryName = recipeCategoryName;
+	}
 	
-}
+	
+	public int getRecipeCategoryNo() {
+		return recipeCategoryNo;
+	}
+	public void setRecipeCategoryNo(int recipeCategoryNo) {
+		this.recipeCategoryNo = recipeCategoryNo;
+	}
+	public String getRecipeCategoryName() {
+		return recipeCategoryName;
+	}
+	public void setRecipeCategoryName(String recipeCategoryName) {
+		this.recipeCategoryName = recipeCategoryName;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "RecipeCategory [recipeCategoryNo=" + recipeCategoryNo + ", recipeCategoryName=" + recipeCategoryName
+				+ "]";
+	}
+	
+	
+}//class.end
