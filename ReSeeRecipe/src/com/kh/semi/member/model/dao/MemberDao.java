d38697b3df5478cb91d3b0fec44ff52039cb3e47
eclipsecandy <kh.rang73@gmail.com>
2023-09-29 03:06:04 +0900

    loginMemberDao만드는중

--- ReSeeRecipe/src/com/kh/semi/member/model/dao/MemberDao.java
+++ ReSeeRecipe/src/com/kh/semi/member/model/dao/MemberDao.java
@@ -1,5 +1,24 @@
 package com.kh.semi.member.model.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class MemberDao {
+	
+	private Properties prop = new Properties();
+	
+	public MemberDao() {
+		
+		String file = MemberDao.class.getResource("sql/member/member-mapper.xml").getPath();
+		
+		try {
+			prop.loadFromXML(new FileInputStream(file));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 }
--- ReSeeRecipe/src/com/kh/semi/member/model/service/MemberService.java
+++ ReSeeRecipe/src/com/kh/semi/member/model/service/MemberService.java
@@ -1,14 +1,23 @@
 package com.kh.semi.member.model.service;
 
+import static com.kh.semi.common.JDBCTemplate.getConnection;
+
 import java.sql.Connection;
 
-import com.kh.semi.common.JDBCTemplate;
+import com.kh.semi.member.model.dao.MemberDao;
 
 public class MemberService {
 	
 	public void loginMember(String memberId, String memberPwd) {
 		
-		Connection conn = JDBCTemplate().getConnection();
+		// Connection 객체 생성
+		Connection conn = getConnection();
+		
+		// DAO호출
+		new MemberDao().loginMember(conn, memberId, memberPwd);
+		
+		
+		
 	}
 
 }
