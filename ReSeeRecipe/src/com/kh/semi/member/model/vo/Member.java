8adc60030f9cfc1ef08d7b258524ead49babc021
eclipsecandy <kh.rang73@gmail.com>
2023-09-28 23:30:33 +0900

    java build path가 안잡힌다..

--- ReSeeRecipe/src/com/kh/semi/member/model/vo/Member.java
+++ ReSeeRecipe/src/com/kh/semi/member/model/vo/Member.java
@@ -2,6 +2,39 @@ package com.kh.semi.member.model.vo;
 
 public class Member {
 	
+//	MEM_NO	NUMBER
+//	MEM_ID	VARCHAR2(15 BYTE)
+//	MEM_PWD	VARCHAR2(20 BYTE)
+//	MEM_NAME	VARCHAR2(20 BYTE)
+//	MEM_NICKNAME	VARCHAR2(25 BYTE)
+//	MEM_EMAIL	VARCHAR2(50 BYTE)
+//	MEM_STATUS	VARCHAR2(1 BYTE)
+//	ENROLL_DATE	DATE
+//	MODIFY_DATE	DATE
+//	DELETE_DATE	DATE
+//	MEM_PICTURE	VARCHAR2(1500 BYTE)
+//	MEM_GRADE	NUMBER
+	
+	private int memNo;
+	private String memId;
+	private String memPwd;
+	private String memName;
+	private String memNickname;
+	private String memEmail;
+	private String memStatus;
+	private Date enrollDate;
+	private Date modifyDate;
+	private Date deleteDate;
+	private String memPicture;
+	private int memGrade;
+	// private String memGradeName;
+	
+	
+	
+	
+	
+	
+	
 
 
 }
