b37a4dfe4e01a9add1d45ddf6e67b16123ffdceb
QueenHyeLim <146050874+QueenHyeLim@users.noreply.github.com>
2023-10-01 17:41:17 +0900

    NoticeDao 기본생성자 - 혜림

--- ReSeeRecipe/src/com/kh/semi/notice/model/dao/NoticeDao.java
+++ ReSeeRecipe/src/com/kh/semi/notice/model/dao/NoticeDao.java
@@ -1,6 +1,23 @@
 package com.kh.semi.notice.model.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class NoticeDao {
+	
+	private Properties prop = new Properties();
+	
+	public NoticeDao() {
+		
+		String fileName = NoticeDao.class.getResource("/sql/notice/notice-mapper.xml").getPath();
+		
+		try {
+			prop.loadFromXML(new FileInputStream(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	
 }
