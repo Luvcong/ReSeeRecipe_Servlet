8a33ddd313b01442106fbe6aead7eaddc1051b56
QueenHyeLim <146050874+QueenHyeLim@users.noreply.github.com>
2023-10-01 18:59:02 +0900

    NoticeDao- selectLIst 완성

--- ReSeeRecipe/src/com/kh/semi/notice/model/dao/NoticeDao.java
+++ ReSeeRecipe/src/com/kh/semi/notice/model/dao/NoticeDao.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Properties;
 
 import com.kh.semi.notice.model.vo.Notice;
+import static com.kh.semi.common.JDBCTemplate.*;
 
 public class NoticeDao {
 	
@@ -33,12 +35,26 @@ public class NoticeDao {
 		
 		String sql = prop.getProperty("selectNoticeList");
 		
-		
-		
-		
-		
-		
-		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				Notice n = new Notice();
+				n.setNoticeNo(rset.getInt("NOTICE_NO"));
+				n.setNoticeTitle(rset.getString("NOTICE_TITLE"));
+				n.setNoticeCon(rset.getString("NOTICE_CON"));
+				n.setNoticeDate(rset.getDate("NOTICE_DATE"));
+				n.setNoticeCount(rset.getInt("NOTICE_COUNT"));
+				n.setNoticeWriter(rset.getString("NOTICE_WRITER"));
+				list.add(n);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
 		return list;
 	}
 	
