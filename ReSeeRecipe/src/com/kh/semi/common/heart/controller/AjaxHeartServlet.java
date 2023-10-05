055dfeec3474c9bc8d343e16eba82087a3990776
JYChloeMok <137197469+JYChloeMok@users.noreply.github.com>
2023-10-05 19:19:13 +0900

    Create Hearts - MJY
    7:19p

--- ReSeeRecipe/src/com/kh/semi/common/heart/controller/AjaxHeartServlet.java
+++ ReSeeRecipe/src/com/kh/semi/common/heart/controller/AjaxHeartServlet.java
@@ -12,11 +12,7 @@ import com.google.gson.Gson;
 import com.kh.semi.member.model.vo.Member;
 
 /**
- * Servlet implementation class HeartServlet
- * 
- * 하트관련 모든 요청을 받아주는 Servlet입니다
- * 매핑값은 *.ht입니다
- * 
+ * Servlet implementation class RecipeServletController
  */
 @WebServlet("*.ht")
 public class AjaxHeartServlet extends HttpServlet {
@@ -35,6 +31,17 @@ public class AjaxHeartServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		/* 
+		 * [ ajax요청 시 인스트럭션 ]
+		 * - heartCount*  : 단일 대상이 받은 총 하트 개수 카운트
+		 * - heartChange* : 해당 유저가 해당 대상에 하트를 눌렀는지 체크 후 하트추가/삭제
+		 * 
+		 * url  : switch-case의 매핑값이름.ht
+		 * data : { htTargetNo : 하트 받은 대상(게시글/유저)의 PK를 같이 넘겨야함 }
+		 * 
+		 */
+		
+		
 		// POST용 Encoding 세팅
 		request.setCharacterEncoding("UTF-8");
 		
@@ -45,12 +52,13 @@ public class AjaxHeartServlet extends HttpServlet {
 		// 매핑문자열 키워드 추출
 		String uri = request.getRequestURI();
 		String mapping = uri.substring(uri.lastIndexOf("/") + 1, uri.lastIndexOf("."));
-		// loginMember 검사
-		// 로그인유저만 요청 가능 (테스트 위해 잠시 블러처리)
+		
+		
+		
 		if((request.getSession().getAttribute("loginMember")) == null) {
 			switch(mapping) {
-				// 성공 시 하트 카운트 수(0 ~ 의 숫자) or 혹시라도 뭔가 일이있어 실패 시 빈문자열
-				/* 단일 대상에 하트가 몇개인지 count : String반환 */
+				// 로그인 안해도 요청 가능한 기능
+				/*************** 특정 대상 하트 카운트 조회 기능 : String => 성공 시 Count 실패 시 빈문자열 *****/
 				case "htCountRecipe" : result = htc.htCountRecipe(request, response); break;
 				/*
 				case "htCountBookmark" : result = htc.htCountBookmark(request, response); break;
@@ -61,23 +69,11 @@ public class AjaxHeartServlet extends HttpServlet {
 		
 			}
 		} else {
-
+			// 로그인유저만 요청 가능한 기능
 			// Controller 분배 구문
 			switch(mapping) {
 			
-				/* 
-				 * - ajax요청 시 인스트럭션
-				 * - heartCount*  : 단일 대상이 받은 총 하트 개수 카운트
-				 * - heartChange* : 해당 유저가 해당 대상에 하트를 눌렀는지 체크 후 하트추가/삭제
-				 * 
-				 * type : 'post'
-				 * url  : switch-case의 매핑값이름.ht
-				 * data : { htTargetNo : 하트 받은 대상(게시글/유저)의 PK를 같이 넘겨야함 }
-				 * 
-				 */
-			
-				
-				/* 하트 추가/삭제 : 1이나 0반환 (SELECT + IN/DEL) */
+				/*************** 하트 추가/삭제 기능 : int => 성공 시 1 실패 시 0 반환 *********************/
 				case "htChangeRecipe" : result = htc.htChangeRecipe(request, response); break;
 				case "htChangeBookmark" : result = htc.htChangeBookmark(request, response); break;
 				case "htChangeNotice" : result = htc.htChangeNotice(request, response); break;
