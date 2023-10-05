ca7ea73221b148b81dc0ab357607792f84dcafa0
QueenHyeLim <146050874+QueenHyeLim@users.noreply.github.com>
2023-10-05 16:46:23 +0900

    MemberDetailManagerController 서블릿 완성 - 혜림

--- ReSeeRecipe/src/com/kh/semi/member/controller/MemberDetailManagerController.java
+++ ReSeeRecipe/src/com/kh/semi/member/controller/MemberDetailManagerController.java
@@ -1,11 +1,16 @@
 package com.kh.semi.member.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.semi.member.model.service.MemberService;
+import com.kh.semi.member.model.vo.Member;
 
 /**
  * Servlet implementation class MemberDetailManagerController
@@ -27,9 +32,17 @@ public class MemberDetailManagerController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		// 값 뽑기 - 회원번호("mno")
+		int memNo = Integer.parseInt(request.getParameter("mno"));
 		
-	
-	
+		// Service호출 회원번호로 해당 회원 정보 SELECT
+		ArrayList<Member> list = new MemberService().selectMemInfo(memNo);
+		
+		// 응답화면 지정
+		request.setAttribute("list", list);
+		request.setAttribute("memNo", memNo);
+		
+		request.getRequestDispatcher("views/member/memberDetailManager.jsp").forward(request, response);
 	
 	}
 
