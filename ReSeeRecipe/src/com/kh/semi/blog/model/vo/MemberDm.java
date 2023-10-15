1703a02b984e16163be1c5d772db0f493091e492
LeeJongHyuk323 <146060547+LeeJongHyuk323@users.noreply.github.com>
2023-10-15 14:44:14 +0900

    커밋

---
+++ ReSeeRecipe/src/com/kh/semi/blog/model/vo/MemberDm.java
@@ -0,0 +1,11 @@
+package src.com.kh.semi.blog.model.vo;
+
+public class MemberDm {
+
+	private int dmNo;
+	private String receiveMem;
+	private String sendMem;
+	private String dmContent;
+	private Date sendDate;
+	
+}
