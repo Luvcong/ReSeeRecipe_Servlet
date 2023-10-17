6806469f43dfc58960ea86ac880a5945e376c0ed
QueenHyeLim <146050874+QueenHyeLim@users.noreply.github.com>
2023-10-17 15:10:18 +0900

    와 관리자 프로필 수정 가능

--- ReSeeRecipe/src/com/kh/semi/manager/controller/ManagerSettingController.java
+++ ReSeeRecipe/src/com/kh/semi/manager/controller/ManagerSettingController.java
@@ -19,7 +19,7 @@ import com.oreilly.servlet.MultipartRequest;
 /**
  * Servlet implementation class ManagerSettingController
  */
-@WebServlet("/hlsettingmanager.ma")
+@WebServlet("/hlsettingmanager.ma") 
 public class ManagerSettingController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
